7454ec58acdab8694d2c073e58fdabf60569d9ef
Stefan Gründel <stefan.gruendel@gmail.com>
2023-05-13 10:08:43 +0200

    adapt localhost URL for IPv4, unique index

--- src/db.js
+++ src/db.js
@@ -2,7 +2,7 @@
 
 const mongoose = require('mongoose');
 mongoose.set('strictQuery', false);
-mongoose.connect('mongodb://localhost:27017/stonks-tradegate');
+mongoose.connect('mongodb://127.0.0.1:27017/stonks-tradegate');
 
 var exports = module.exports = {};
 
@@ -43,6 +43,6 @@ const transactions = new mongoose.Schema({
     autoCreate: true,
     timestamps: true,
 });
-transactions.index({ isin: 1, date: -1, time: 1 });
-transactions.index({ isin: 1, date: -1, id: -1 });
+transactions.index({ isin: 1, date: -1, time: -1 }, { unique: true });
+transactions.index({ isin: 1, date: -1, id: -1 }, { unique: true });
 exports.Transactions = mongoose.model('Transactions', transactions);
