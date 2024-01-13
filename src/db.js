08748368fa515a2e7e3deab3aeaf0d35d2202300
Stefan Gründel <stefan.gruendel@gmail.com>
2024-01-13 09:32:11 +0100

    update mongoose

--- src/db.js
+++ src/db.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const mongoose = require('mongoose');
-mongoose.set('strictQuery', false);
 mongoose.connect('mongodb://127.0.0.1:27017/stonks-tradegate');
 
 var exports = module.exports = {};
